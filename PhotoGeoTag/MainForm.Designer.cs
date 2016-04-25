4f98fc32378b5232c61a5ae220fb348f806ce437
netcharm <netcharm@163.com>
2016-04-25 10:49:59 +0800

    Initialized solution

---
+++ PhotoGeoTag/MainForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace PhotoGeoTag
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMap = new GMap.NET.WindowsForms.GMapControl();
+            this.cbMapProviders = new System.Windows.Forms.ComboBox();
+            this.status = new System.Windows.Forms.StatusStrip();
+            this.tsLat = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsLon = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsZoom = new System.Windows.Forms.ToolStripStatusLabel();
+            this.trackZoom = new System.Windows.Forms.TrackBar();
+            this.picGeoRef = new System.Windows.Forms.PictureBox();
+            this.chkMapShift = new System.Windows.Forms.CheckBox();
+            this.status.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackZoom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGeoRef)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gMap
+            // 
+            this.gMap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gMap.Bearing = 0F;
+            this.gMap.CanDragMap = true;
+            this.gMap.EmptyTileColor = System.Drawing.Color.LightGray;
+            this.gMap.GrayScaleMode = false;
+            this.gMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMap.LevelsKeepInMemmory = 5;
+            this.gMap.Location = new System.Drawing.Point(13, 13);
+            this.gMap.MarkersEnabled = true;
+            this.gMap.MaxZoom = 18;
+            this.gMap.MinZoom = 0;
+            this.gMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMap.Name = "gMap";
+            this.gMap.NegativeMode = false;
+            this.gMap.PolygonsEnabled = true;
+            this.gMap.RetryLoadTile = 0;
+            this.gMap.RoutesEnabled = true;
+            this.gMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMap.ShowTileGridLines = false;
+            this.gMap.Size = new System.Drawing.Size(615, 421);
+            this.gMap.TabIndex = 0;
+            this.gMap.Zoom = 2D;
+            this.gMap.OnPositionChanged += new GMap.NET.PositionChanged(this.gMap_OnPositionChanged);
+            this.gMap.OnMapZoomChanged += new GMap.NET.MapZoomChanged(this.gMap_OnMapZoomChanged);
+            this.gMap.OnMapTypeChanged += new GMap.NET.MapTypeChanged(this.gMap_OnMapTypeChanged);
+            // 
+            // cbMapProviders
+            // 
+            this.cbMapProviders.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbMapProviders.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMapProviders.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cbMapProviders.FormattingEnabled = true;
+            this.cbMapProviders.Location = new System.Drawing.Point(12, 452);
+            this.cbMapProviders.Name = "cbMapProviders";
+            this.cbMapProviders.Size = new System.Drawing.Size(160, 20);
+            this.cbMapProviders.Sorted = true;
+            this.cbMapProviders.TabIndex = 1;
+            this.cbMapProviders.SelectedIndexChanged += new System.EventHandler(this.cbMapProviders_SelectedIndexChanged);
+            // 
+            // status
+            // 
+            this.status.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.status.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsLat,
+            this.tsLon,
+            this.tsZoom});
+            this.status.Location = new System.Drawing.Point(0, 487);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(640, 22);
+            this.status.TabIndex = 2;
+            this.status.Text = "OK";
+            // 
+            // tsLat
+            // 
+            this.tsLat.AutoSize = false;
+            this.tsLat.AutoToolTip = true;
+            this.tsLat.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.tsLat.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.tsLat.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsLat.Name = "tsLat";
+            this.tsLat.Size = new System.Drawing.Size(120, 17);
+            this.tsLat.Text = "Lat:";
+            this.tsLat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tsLon
+            // 
+            this.tsLon.AutoSize = false;
+            this.tsLon.AutoToolTip = true;
+            this.tsLon.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.tsLon.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.tsLon.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsLon.Name = "tsLon";
+            this.tsLon.Size = new System.Drawing.Size(120, 17);
+            this.tsLon.Text = "Lon:";
+            this.tsLon.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tsZoom
+            // 
+            this.tsZoom.AutoSize = false;
+            this.tsZoom.AutoToolTip = true;
+            this.tsZoom.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.tsZoom.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.tsZoom.Name = "tsZoom";
+            this.tsZoom.Size = new System.Drawing.Size(96, 17);
+            this.tsZoom.Text = "Zoom:";
+            this.tsZoom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // trackZoom
+            // 
+            this.trackZoom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackZoom.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackZoom.LargeChange = 2;
+            this.trackZoom.Location = new System.Drawing.Point(178, 441);
+            this.trackZoom.Name = "trackZoom";
+            this.trackZoom.Size = new System.Drawing.Size(236, 42);
+            this.trackZoom.TabIndex = 4;
+            this.trackZoom.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackZoom.Scroll += new System.EventHandler(this.trackZoom_Scroll);
+            // 
+            // picGeoRef
+            // 
+            this.picGeoRef.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.picGeoRef.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picGeoRef.Location = new System.Drawing.Point(574, 441);
+            this.picGeoRef.Name = "picGeoRef";
+            this.picGeoRef.Size = new System.Drawing.Size(54, 42);
+            this.picGeoRef.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picGeoRef.TabIndex = 5;
+            this.picGeoRef.TabStop = false;
+            this.picGeoRef.DragDrop += new System.Windows.Forms.DragEventHandler(this.picGeoRef_DragDrop);
+            this.picGeoRef.DragEnter += new System.Windows.Forms.DragEventHandler(this.picGeoRef_DragEnter);
+            // 
+            // chkMapShift
+            // 
+            this.chkMapShift.AutoSize = true;
+            this.chkMapShift.Location = new System.Drawing.Point(424, 454);
+            this.chkMapShift.Name = "chkMapShift";
+            this.chkMapShift.Size = new System.Drawing.Size(108, 16);
+            this.chkMapShift.TabIndex = 6;
+            this.chkMapShift.Text = "Map Need Shift";
+            this.chkMapShift.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 509);
+            this.Controls.Add(this.chkMapShift);
+            this.Controls.Add(this.picGeoRef);
+            this.Controls.Add(this.trackZoom);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.cbMapProviders);
+            this.Controls.Add(this.gMap);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.Text = "Maps";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.status.ResumeLayout(false);
+            this.status.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackZoom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGeoRef)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMap;
+        private System.Windows.Forms.ComboBox cbMapProviders;
+        private System.Windows.Forms.StatusStrip status;
+        private System.Windows.Forms.ToolStripStatusLabel tsLat;
+        private System.Windows.Forms.ToolStripStatusLabel tsLon;
+        private System.Windows.Forms.TrackBar trackZoom;
+        private System.Windows.Forms.ToolStripStatusLabel tsZoom;
+        private System.Windows.Forms.PictureBox picGeoRef;
+        private System.Windows.Forms.CheckBox chkMapShift;
+    }
+}
+
---
+++ PhotoGeoTag/MainForm.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms.ToolTips;
+
+namespace PhotoGeoTag
+{
+    public partial class MainForm : Form
+    {
+        string AppFolder = Path.GetDirectoryName(Application.ExecutablePath);
+        string CacheFolder = "";
+
+        Dictionary<string, Guid> mapSource = new Dictionary<string, Guid>();
+
+        MarsWGS PosShift = new MarsWGS();
+        bool MapShift = false;
+
+        GMapOverlay OverlayRefPos = new GMapOverlay("RefPos");
+        GMapOverlay OverlayPhotos = new GMapOverlay("Photos");
+        GMapOverlay OverlayPoints = new GMapOverlay("Points");
+        GMapOverlay OverlayTracks = new GMapOverlay("Tracks");
+        GMapOverlay OverlayRoutes = new GMapOverlay("Routes");
+
+        GMapOverlay OverlayRefPosWGS = new GMapOverlay("RefPos");
+        GMapOverlay OverlayPhotosWGS = new GMapOverlay("Photos");
+        GMapOverlay OverlayPointsWGS = new GMapOverlay("Points");
+        GMapOverlay OverlayTracksWGS = new GMapOverlay("Tracks");
+        GMapOverlay OverlayRoutesWGS = new GMapOverlay("Routes");
+
+        GMapOverlay OverlayRefPosMAR = new GMapOverlay("RefPos");
+        GMapOverlay OverlayPhotosMAR = new GMapOverlay("Photos");
+        GMapOverlay OverlayPointsMAR = new GMapOverlay("Points");
+        GMapOverlay OverlayTracksMAR = new GMapOverlay("Tracks");
+        GMapOverlay OverlayRoutesMAR = new GMapOverlay("Routes");
+
+        private void updatePositions( GMapOverlay overlay )
+        {
+            string mapName = gMap.MapProvider.Name;
+            //MapShift = chkMapShift.Checked;
+            if ( mapName.StartsWith( "GoogleChina" ) || mapName.StartsWith( "BingMap" ) )
+            {
+                MapShift = true;
+                overlay.Markers.Clear();
+                if ( string.Equals( overlay.Id, "RefPos", StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    foreach ( GMarkerGoogle marker in OverlayRefPosMAR.Markers )
+                        overlay.Markers.Add( marker );
+                }
+                else if ( string.Equals( overlay.Id, "Photos", StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    foreach ( GMarkerGoogle marker in OverlayPhotosMAR.Markers )
+                        overlay.Markers.Add( marker );
+                }
+                else if ( string.Equals( overlay.Id, "Points", StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    foreach ( GMarkerGoogle marker in OverlayPointsMAR.Markers )
+                        overlay.Markers.Add( marker );
+                }
+                else if ( string.Equals( overlay.Id, "Tracks", StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    foreach ( GMarkerGoogle marker in OverlayTracksMAR.Markers )
+                        overlay.Markers.Add( marker );
+                }
+                else if ( string.Equals( overlay.Id, "Routes", StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    foreach ( GMarkerGoogle marker in OverlayRoutesMAR.Markers )
+                        overlay.Markers.Add( marker );
+                }
+            }
+            else
+            {
+                MapShift = false;
+                overlay.Markers.Clear();
+                if ( string.Equals( overlay.Id, "RefPos", StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    foreach ( GMarkerGoogle marker in OverlayRefPosWGS.Markers )
+                        overlay.Markers.Add( marker );
+                }
+                else if ( string.Equals( overlay.Id, "Photos", StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    foreach ( GMarkerGoogle marker in OverlayPhotosWGS.Markers )
+                        overlay.Markers.Add( marker );
+                }
+                else if ( string.Equals( overlay.Id, "Points", StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    foreach ( GMarkerGoogle marker in OverlayPointsWGS.Markers )
+                        overlay.Markers.Add( marker );
+                }
+                else if ( string.Equals( overlay.Id, "Tracks", StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    foreach ( GMarkerGoogle marker in OverlayTracksWGS.Markers )
+                        overlay.Markers.Add( marker );
+                }
+                else if ( string.Equals( overlay.Id, "Routes", StringComparison.CurrentCultureIgnoreCase ) )
+                {
+                    foreach ( GMarkerGoogle marker in OverlayRoutesWGS.Markers )
+                        overlay.Markers.Add( marker );
+                }
+
+            }
+        }
+
+        private void updatePositions()
+        {
+            foreach ( GMapOverlay overlay in gMap.Overlays )
+            {
+                updatePositions( overlay );
+            }
+        }
+
+        private Bitmap getPhotoThumb(Bitmap image)
+        {
+            int w=64, h=64;
+            double ar = (double)image.Width / (double)image.Height;
+            if ( ar > 1 )
+            {
+                h = (int) ( w / ar );
+            }
+            else if ( ar < 1 )
+            {
+                w = (int) ( h * ar );
+            }
+            return ( new Bitmap( image, w, h ) );
+        }
+
+        private Bitmap getPhotoThumb(Image image)
+        {
+            return ( getPhotoThumb( (Bitmap) image ) );
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Application.EnableVisualStyles();
+            Icon = Icon.ExtractAssociatedIcon( Application.ExecutablePath );
+        }
+
+        private void Form1_Load( object sender, EventArgs e )
+        {
+            CacheFolder = AppFolder + Path.DirectorySeparatorChar + "Cache";
+            
+            cbMapProviders.Items.Clear();
+            cbMapProviders.BeginUpdate();
+            foreach ( GMapProvider map in GMapProviders.List )
+            {
+                if ( map.Name.StartsWith( "Bing" ) ||
+                     map.Name.StartsWith( "Google" ) ||
+                     //map.Name.StartsWith( "Yahoo" ) ||
+                     map.Name.StartsWith( "Open" ) )
+                {
+                    cbMapProviders.Items.Add( map.Name );
+                    mapSource.Add( map.Name, map.Id );
+                }
+            }
+            cbMapProviders.EndUpdate();
+            cbMapProviders.SelectedIndex = cbMapProviders.Items.IndexOf( "GoogleChinaHybridMap" );
+            if(Directory.Exists( CacheFolder ))
+            {
+                Directory.CreateDirectory( CacheFolder );
+            }
+            trackZoom.Minimum = 2;
+            trackZoom.Maximum = 20;
+            trackZoom.Value = 12;
+
+            picGeoRef.AllowDrop = true;
+
+            gMap.Manager.BoostCacheEngine = true;
+            gMap.Manager.CacheOnIdleRead = true;
+            gMap.Manager.UseDirectionsCache = true;
+            gMap.Manager.UseGeocoderCache = true;
+            gMap.Manager.UseMemoryCache = true;
+            gMap.Manager.UsePlacemarkCache = true;
+            gMap.Manager.UseRouteCache = true;
+            gMap.Manager.UseUrlCache = true;
+            gMap.CanDragMap = true;
+            gMap.DragButton = MouseButtons.Left;
+            gMap.FillEmptyTiles = true;
+            gMap.MapScaleInfoEnabled = true;
+            gMap.Manager.Mode = AccessMode.ServerAndCache;
+            gMap.MaxZoom = trackZoom.Maximum;
+            gMap.MinZoom = trackZoom.Minimum;
+            //gMap.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
+            gMap.RetryLoadTile = 5;
+            gMap.ScaleMode = ScaleModes.Fractional;
+            gMap.ScalePen = Pens.AntiqueWhite;
+            gMap.ShowCenter = false;
+            //GeocodingProvider
+
+            gMap.CacheLocation = CacheFolder;
+            gMap.MapProvider = GMapProviders.TryGetProvider( mapSource["GoogleChinaHybridMap"] );
+            //gMap.MapProvider = GoogleChinaHybridMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            gMap.SetPositionByKeywords( "Sanya,China" );
+            gMap.Zoom = trackZoom.Value;
+
+            gMap.Overlays.Add( OverlayRefPos );
+            gMap.Overlays.Add( OverlayPhotos );
+            gMap.Overlays.Add( OverlayPoints );
+            gMap.Overlays.Add( OverlayTracks );
+            gMap.Overlays.Add( OverlayRoutes );
+        }
+
+        private void cbMapProviders_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            //RectLatLng? latlng = gMap.BoundsOfMap;
+            PointLatLng CurrentPos = gMap.Position;
+            gMap.MapProvider = GMapProviders.TryGetProvider( mapSource[cbMapProviders.SelectedItem.ToString()] );
+            //gMap.BoundsOfMap = latlng;
+        }
+
+        private void MainForm_FormClosing( object sender, FormClosingEventArgs e )
+        {
+            gMap.MapProvider = EmptyProvider.Instance;
+            gMap.MapProvider.BypassCache = true;
+            gMap.Manager.CancelTileCaching();
+        }
+
+        private void trackZoom_Scroll( object sender, EventArgs e )
+        {
+            gMap.Zoom = trackZoom.Value;
+            tsZoom.Text = $"Zoom: {gMap.Zoom.ToString()}";
+            tsZoom.ToolTipText = tsZoom.Text;           
+        }
+
+        private void gMap_OnMapZoomChanged()
+        {
+            trackZoom.Value = (int)gMap.Zoom;
+            tsZoom.Text = $"Zoom: {gMap.Zoom.ToString()}";
+            tsZoom.ToolTipText = tsZoom.Text;
+        }
+
+        private void gMap_OnPositionChanged( PointLatLng point )
+        {
+            double lat = point.Lat;
+            double lng = point.Lng;
+            
+            if ( MapShift)
+            {
+                PosShift.Convert2WGS( point.Lng, point.Lat, out lng, out lat );
+            }
+            string refLat = lat < 0 ? "S" : "N";
+            string refLng = lng < 0 ? "W" : "E";
+            tsLat.Text = $"Lat: {lat.ToString( "F6" )} {refLat}";
+            tsLon.Text = $"Lon: {lng.ToString( "F6" )} {refLng}";
+        }
+
+        private void picGeoRef_DragEnter( object sender, DragEventArgs e )
+        {
+            e.Effect = DragDropEffects.Link;
+        }
+
+        private void picGeoRef_DragDrop( object sender, DragEventArgs e )
+        {
+            PointLatLng pos = gMap.Position;
+
+            string[] flist = (string[])e.Data.GetData( DataFormats.FileDrop, true );
+            picGeoRef.Load( flist[0] );
+
+            pos.Lat = ImageGeoTag.GetLatitude( picGeoRef.Image, pos.Lat );
+            pos.Lng = ImageGeoTag.GetLongitude( picGeoRef.Image, pos.Lng );
+
+            double lat = pos.Lat, lng = pos.Lng;
+            PosShift.Convert2Mars( pos.Lng, pos.Lat, out lng, out lat );
+
+            OverlayRefPosWGS.Markers.Clear();
+            //OverlayRefPosWGS.Markers.Add( new GMarkerGoogle( pos, GMarkerGoogleType.blue_pushpin ) );
+            OverlayRefPosWGS.Markers.Add( new GMarkerGoogle( pos, getPhotoThumb( picGeoRef.Image ) ) );
+
+            OverlayRefPosMAR.Markers.Clear();
+            //OverlayRefPosMAR.Markers.Add( new GMarkerGoogle( new PointLatLng(lat, lng), GMarkerGoogleType.blue_pushpin ) );
+            OverlayRefPosMAR.Markers.Add( new GMarkerGoogle( new PointLatLng( lat, lng ), getPhotoThumb( picGeoRef.Image ) ) );
+
+            gMap.Zoom = 12;
+
+            updatePositions();
+        }
+
+        private void gMap_OnMapTypeChanged( GMapProvider type )
+        {
+            //
+            updatePositions();
+        }
+    }
+}
