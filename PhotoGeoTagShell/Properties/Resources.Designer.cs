9dfe0ed75ceed61b18e22ab7d1dc113bb239d6e6
netcharm <netcharm@163.com>
2016-05-23 18:14:39 +0800

    update application resources & version number

--- PhotoGeoTagShell/Properties/Resources.Designer.cs
+++ PhotoGeoTagShell/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace PhotoGeoTagShell.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap search_button_icon_png_24 {
+            get {
+                object obj = ResourceManager.GetObject("search_button_icon_png_24", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap search_poi_png_24 {
             get {
                 object obj = ResourceManager.GetObject("search_poi_png_24", resourceCulture);
